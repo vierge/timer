fabb6200ea512a26452f317dd050b981d07482e7
vierge <marancholia@gmail.com>
2020-01-14 17:09:37 +0000

    implemented timer.js and did not skip ahead this time :3

---
+++ timer.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+
+
+const alarm = args.forEach(value => {
+    if (value => 0) {
+    setTimeout(() => {
+      process.stdout.write('.');
+    }, (value * 1000))
+  }
+});
